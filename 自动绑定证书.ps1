f1a3bfd55ae1938f5f3f980cd63cc9b5381f9fef
SkybowAlexandra <1973659295@qq.com>
2023-10-21 17:39:43 +0800

    commit 1

--- 自动绑定证书.ps1
+++ 自动绑定证书.ps1
@@ -3,9 +3,22 @@ $webname = "脚本测试"
 
 #绑定的域名的信息
 $HostBind = 
-"*:46889:yfcbsf991.qpwbjkn.top",
-"*:45125:fvayzsf991.xxmffei.top.pfx"
+"*:44994:wvfq.pwxnntkp.top",
+"*:19923:jqjh.retbhdz.top",
+"*:18441:eetp.hydjqc.top",
+"*:23799:ghown.lvtveqvn.top",
+"*:53965:cgyg.goucwm.top",
+"*:20932:rvfaze.woaziwn.top",
+"*:23515:udpoi.vtgxlt.top",
+"*:26486:dsns.riensck.top",
+"*:29481:gtpu.ekppqlvm.top",
+"*:40619:lme.xullarp.top",
+"*:38774:grx.sbsmsk.top",
+"*:24658:hhvw.gefczasmw.top",
+"*:21650:qbaxk.ckmetbvl.top",
+"*:32147:glbrb.prdenaktd.top"
 
+#使用:分割字符串
 $hostData = $HostBind -split ":"
 
 
@@ -43,10 +56,6 @@ function GetCrtPassMsg($Passfile)
     return $CrtMsg
 }
 
-
-
-
-
 #通过证书密码本,模糊匹配某个路径下的证书,并返回一个字典
 function Crtmatch
 {
@@ -54,21 +63,57 @@ function Crtmatch
         [hashtable]$CrtMsg,
         [array]$DirCrts
     )
+    
+    #存放有匹配的信息
     $FinalHashTable =@{}
+    #存放无匹配的信息
+    $UnmatchedHashTable = @{}
+
     foreach ($key in $CrtMsg.Keys) 
     {
+        $matched = $false
         foreach ($value in $DirCrts)
          {
             if ($value -like "*$key*")
             {
-                $src_value =  $CrtMsg[$key]
-                $FinalHashTable[$value] = $src_value
+                #有密码信息,在数组里找到了证书
+                $FinalHashTable[$value] = $CrtMsg[$key]
+                #标记找到匹配的证书
+                $matched = $true  
             }
         }
+        if (-not $matched)
+        {
+            # 没有找到匹配的证书，将其存储在不匹配的哈希表中
+            $UnmatchedHashTable[$key] = $CrtMsg[$key]
+        }
+
     }
+    Write-Host "以下域名有密码但找不到任何证书:.... " -ForegroundColor Red
+    foreach ($it in $UnmatchedHashTable.Keys)
+    {
+        Write-Host $it
+    }
+
     return $FinalHashTable
 }
 
+function FindValuesNotInDictionary
+{
+    param (
+        [array]$Array,
+        [hashtable]$Dictionary
+    )
+    $NotInDictionary = @()
+    foreach ($item in $Array)
+    {
+        if (-not $Dictionary.ContainsKey($item))
+        {
+            $NotInDictionary += $item
+        }
+    }
+    return $NotInDictionary
+}
 
 function main
 {
@@ -76,27 +121,60 @@ function main
     #获取当前密码本的信息,返回一个字典
     $CrtMsg = GetCrtPassMsg(".\password.txt")
 
-    #获取当前目录的所有证书文件,返回一个数组,
+    #获取当前目录的所有证书文件,返回一个数组,存放所有证书文件名
     $DirCrts = Get-PfxCertificateFileNames(".\")
 
-    #匹配本目录证书文件，获取最终的字典
+
+
+    #匹配本目录证书文件，获取最终的字典,存放证书文件名和密码
     $FinalHashTable =  Crtmatch -CrtMsg $CrtMsg -DirCrts $DirCrts
 
+    #找到有证书但是没有密码的
+    $notFoundValues = FindValuesNotInDictionary -Array $DirCrts -Dictionary $FinalHashTable
+    Write-Host "未找到证书密码的如下" -ForegroundColor Red
+    foreach($it in $notFoundValues)
+    {
+        Write-Host $it
+    }
 
+
+
+
+
+    #定义一个字典保存域名和证书指纹
+    $HostAndThumb = @{}
+
+    $index=1
+    Write-Host "已找到证书密码如下,导入以下证书:" -ForegroundColor Green
     $FinalHashTable.GetEnumerator()|ForEach-Object{
-        Write-Host "CrtName: $($_.Key), Passworld: $($_.Value)"
+        
+        Write-Host "id: $index  CrtName: $($_.Key)  Password: $($_.Value)"
+        $index++
         #执行导入证书
         $Pwd = ConvertTo-SecureString -String $($_.Value) -Force -AsPlainText
         $cer = Import-PfxCertificate -FilePath $($_.Key) -CertStoreLocation Cert:\LocalMachine\My -Password $Pwd
+        #保存证书指纹信息
+        $HostAndThumb[$cer.GetName()] = $cer.Thumbprint     
     }
 
-    #执行绑定站点
+    $index=1
+    Write-Host "绑定站点信息如下:" -ForegroundColor Green
     for ($x = 0; $x -lt $hostData.Length; $x = $x + 3)
     {
-	    New-WebBinding -Name $webname -Protocol https -Port $hostData[$x + 1] -IPAddress $hostData[$x] -HostHeader $hostData[$x + 2] -SslFlags 1
-	    (Get-WebBinding -Name $webname -Port $hostData[$x + 1] -Protocol "https" -IPAddress $hostData[$x] -HostHeader $hostData[$x + 2]).AddSslCertificate($cer.Thumbprint, "my")
-    }
 
+        if([string]::IsNullOrEmpty($HostAndThumb["CN="+$hostData[$x + 2]]))
+        {
+            Write-Host "该绑定域名找不到证书": $hostData[$x+2]
+            continue
+        }
+        
+        #根据域名获得指纹信息
+        Write-Host $index ":" $hostData[$x+2]: $HostAndThumb["CN="+$hostData[$x + 2]]
+	    #New-WebBinding -Name $webname -Protocol https -Port $hostData[$x + 1] -IPAddress $hostData[$x] -HostHeader $hostData[$x + 2] -SslFlags 1
+	    #(Get-WebBinding -Name $webname -Port $hostData[$x + 1] -Protocol "https" -IPAddress $hostData[$x] -HostHeader $hostData[$x + 2]).AddSslCertificate($HostAndThumb["CN="+$hostData[$x + 2]], "my")
+        $index++
+    }
+    Write-Host "执行完毕" -ForegroundColor Green
 }
 
 main
